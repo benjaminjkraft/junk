33921fed579619ad258ebf7923f8ccba63100412
Ben Kraft <ben@benkraft.org>
2019-08-04 17:09:37 -0700

    the dreaded import *

--- pickle_quine.py
+++ pickle_quine.py
@@ -1,17 +1,17 @@
 #!/usr/bin/env python3
-import pickle
+from pickle import *
 import struct
 
 
 # So here's the plan.  We're going to make a pickle which will consist of two
-# parts, each of which will be repeated twice to form the whole pickle.
+# parts, each of which will be repeated twice to form the whole 
 
 # Part 1 is pretty simple: it consists of the pickle version 3 header
 # followed by the preamble to the string consisting of part 1 followed by part
 # 2.  (We'll fill in its length later.)
 PART_1 = (
-    pickle.PROTO, b'\x03',                      # pickle version 3 header
-    pickle.SHORT_BINBYTES,                      # bytestring (length TBD)
+    PROTO, b'\x03',                             # pickle version 3 header
+    SHORT_BINBYTES,                             # bytestring (length TBD)
 )
 
 # After part 1 (plus one byte of length) executes, our stack will have a single
@@ -28,53 +28,53 @@ PART_1 = (
 # and we'll then add those up with operator.add.
 # So here we go:
 PART_2 = (
-    pickle.BINPUT, b'\x00',                     # store the string in memo 0
+    BINPUT, b'\x00',                            # store the string in memo 0
 
-    pickle.GLOBAL, b'operator\ngetitem\n',      # put operator.getitem on stack
-    pickle.BINPUT, b'\x01',                     # store it in memo 1
+    GLOBAL, b'operator\ngetitem\n',             # put operator.getitem on stack
+    BINPUT, b'\x01',                            # store it in memo 1
 
-    pickle.GLOBAL, b'builtins\nslice\n',        # put slice on stack
-    pickle.BINPUT, b'\x02',                     # store it in memo 2
+    GLOBAL, b'builtins\nslice\n',               # put slice on stack
+    BINPUT, b'\x02',                            # store it in memo 2
 
-    pickle.GLOBAL, b'operator\nadd\n',          # put operator.add on stack
-    pickle.BINPUT, b'\x03',                     # store that in memo 3
+    GLOBAL, b'operator\nadd\n',                 # put operator.add on stack
+    BINPUT, b'\x03',                            # store that in memo 3
 
-    pickle.BINGET, b'\x02',                     # load slice from memo 2
-    pickle.BININT1, b'\x00',                    # put 0 on stack
-    pickle.BININT1, b'\x04',                    # put 4 on stack
-    pickle.TUPLE2, pickle.REDUCE,               # call --> slice(0, 4)
-    pickle.BINPUT, b'\x04',                     # store that in memo 4
+    BINGET, b'\x02',                            # load slice from memo 2
+    BININT1, b'\x00',                           # put 0 on stack
+    BININT1, b'\x04',                           # put 4 on stack
+    TUPLE2, REDUCE,                             # call --> slice(0, 4)
+    BINPUT, b'\x04',                            # store that in memo 4
 
-    pickle.BINGET, b'\x02',                     # load slice from memo 2
-    pickle.BININT1, b'\x04',                    # put 4 on stack
-    pickle.NONE,                                # put None on stack
-    pickle.TUPLE2, pickle.REDUCE,               # call --> slice(4, None)
-    pickle.BINPUT, b'\x05',                     # store that in memo 5
+    BINGET, b'\x02',                            # load slice from memo 2
+    BININT1, b'\x04',                           # put 4 on stack
+    NONE,                                       # put None on stack
+    TUPLE2, REDUCE,                             # call --> slice(4, None)
+    BINPUT, b'\x05',                            # store that in memo 5
 
-    pickle.BINGET, b'\x01',                     # load getitem from memo 1
-    pickle.BINGET, b'\x00',                     # load the string from memo 0
-    pickle.BINGET, b'\x04',                     # load slice(0, 4) from memo 4
-    pickle.TUPLE2, pickle.REDUCE,               # tuple + call --> string[:4]
-    pickle.BINPUT, b'\x06',                     # store that in memo 6
+    BINGET, b'\x01',                            # load getitem from memo 1
+    BINGET, b'\x00',                            # load the string from memo 0
+    BINGET, b'\x04',                            # load slice(0, 4) from memo 4
+    TUPLE2, REDUCE,                             # tuple + call --> string[:4]
+    BINPUT, b'\x06',                            # store that in memo 6
 
-    pickle.BINGET, b'\x01',                     # load getitem from memo again
-    pickle.BINGET, b'\x00',                     # load the string from memo 0
-    pickle.BINGET, b'\x05',                     # slice(4, None) from memo 5
-    pickle.TUPLE2, pickle.REDUCE,               # tuple + call --> string[4:]
-    pickle.BINPUT, b'\x07',                     # store that in memo 7
+    BINGET, b'\x01',                            # load getitem from memo again
+    BINGET, b'\x00',                            # load the string from memo 0
+    BINGET, b'\x05',                            # slice(4, None) from memo 5
+    TUPLE2, REDUCE,                             # tuple + call --> string[4:]
+    BINPUT, b'\x07',                            # store that in memo 7
 
-    pickle.BINGET, b'\x03',                     # load operator.add from memo 3
-    pickle.BINGET, b'\x06',                     # load string[:4] from memo 6
-    pickle.BINGET, b'\x00',                     # load string from memo 0
-    pickle.TUPLE2, pickle.REDUCE,               # build tuple, call +
-    pickle.BINPUT, b'\x08',                     # store that in memo 8
+    BINGET, b'\x03',                            # load operator.add from memo 3
+    BINGET, b'\x06',                            # load string[:4] from memo 6
+    BINGET, b'\x00',                            # load string from memo 0
+    TUPLE2, REDUCE,                             # build tuple, call +
+    BINPUT, b'\x08',                            # store that in memo 8
 
-    pickle.BINGET, b'\x03',                     # load operator.add from memo 3
-    pickle.BINGET, b'\x08',                     # string[:4] + string from memo
-    pickle.BINGET, b'\x07',                     # load string[4:] from memo 7
-    pickle.TUPLE2, pickle.REDUCE,               # build tuple, call + again
+    BINGET, b'\x03',                            # load operator.add from memo 3
+    BINGET, b'\x08',                            # string[:4] + string from memo
+    BINGET, b'\x07',                            # load string[4:] from memo 7
+    TUPLE2, REDUCE,                             # build tuple, call + again
 
-    pickle.STOP,                                # STOP
+    STOP,                                       # STOP
 )
 
 
@@ -93,46 +93,46 @@ def make_pickle(golfed):
 # can be harder to see what is the argument to what.  We also use MEMOIZE,
 # which is shorthand for BINPUT [next unused value].
 GOLFED_PART_2 = (
-    pickle.MEMOIZE,                             # store the string in memo 0
+    MEMOIZE,                                    # store the string in memo 0
 
     # Here we use an extra trick: since we load two globals from operator we
     # can save a few bytes by storing 'operator' only once, and calling
     # STACK_GLOBAL (which is otherwise marginally less efficient) twice.
-    pickle.SHORT_BINSTRING, b'\x08', b'operator',
-    pickle.MEMOIZE,                             # store it in memo 1
+    SHORT_BINSTRING, b'\x08', b'operator',
+    MEMOIZE,                                    # store it in memo 1
 
-    pickle.SHORT_BINSTRING, b'\x03', b'add',
-    pickle.STACK_GLOBAL,                        # operator.add
-    pickle.DUP,                                 # copy it
+    SHORT_BINSTRING, b'\x03', b'add',
+    STACK_GLOBAL,                               # operator.add
+    DUP,                                        # copy it
 
-    pickle.BINGET, b'\x01',                     # operator
-    pickle.SHORT_BINSTRING, b'\x07', b'getitem',
-    pickle.STACK_GLOBAL,                        # operator.getitem
-    pickle.MEMOIZE,                             # store it in memo 2
+    BINGET, b'\x01',                            # operator
+    SHORT_BINSTRING, b'\x07', b'getitem',
+    STACK_GLOBAL,                               # operator.getitem
+    MEMOIZE,                                    # store it in memo 2
 
-    pickle.BINGET, b'\x00',                     # the string
-    pickle.GLOBAL, b'builtins\nslice\n',        # slice
-    pickle.MEMOIZE,                             # store it in memo 3
+    BINGET, b'\x00',                            # the string
+    GLOBAL, b'builtins\nslice\n',               # slice
+    MEMOIZE,                                    # store it in memo 3
     # The canonical arg here would be 0, but None is valid and saves a byte.
-    pickle.NONE,                                # None
-    pickle.BININT1, b'\x04',                    # 4
-    pickle.TUPLE2, pickle.REDUCE,               # call slice --> slice(None, 4)
-    pickle.TUPLE2, pickle.REDUCE,               # call getitem --> string[:4]
+    NONE,                                       # None
+    BININT1, b'\x04',                           # 4
+    TUPLE2, REDUCE,                             # call slice --> slice(None, 4)
+    TUPLE2, REDUCE,                             # call getitem --> string[:4]
 
-    pickle.BINGET, b'\x00',                     # the string
-    pickle.TUPLE2, pickle.REDUCE,               # call add
+    BINGET, b'\x00',                            # the string
+    TUPLE2, REDUCE,                             # call add
 
-    pickle.BINGET, b'\x02',                     # getitem
-    pickle.BINGET, b'\x00',                     # the string
-    pickle.BINGET, b'\x03',                     # slice
-    pickle.BININT1, b'\x04',                    # 4
-    pickle.NONE,                                # None
-    pickle.TUPLE2, pickle.REDUCE,               # call slice --> slice(4, None)
-    pickle.TUPLE2, pickle.REDUCE,               # call getitem --> string[4:]
+    BINGET, b'\x02',                            # getitem
+    BINGET, b'\x00',                            # the string
+    BINGET, b'\x03',                            # slice
+    BININT1, b'\x04',                           # 4
+    NONE,                                       # None
+    TUPLE2, REDUCE,                             # call slice --> slice(4, None)
+    TUPLE2, REDUCE,                             # call getitem --> string[4:]
 
-    pickle.TUPLE2, pickle.REDUCE,               # call add
+    TUPLE2, REDUCE,                             # call add
 
-    pickle.STOP,                                # STOP
+    STOP,                                       # STOP
 )
 
 
@@ -146,7 +146,7 @@ def make_golfed_pickle():
 
 
 def check_pickle(data):
-    assert data == pickle.loads(data)
+    assert data == loads(data)
 
 
 def main(filename, golfed):
