14ed4d9a716417ec35d8e60ea715dbd65402bf3e
Ben Kraft <ben@benkraft.org>
2019-08-04 17:24:41 -0700

    a bit of reordering, and using NONE in the ungolfed one

--- pickle_quine.py
+++ pickle_quine.py
@@ -28,53 +28,53 @@ PART_1 = (
 # and we'll then add those up with operator.add.
 # So here we go:
 PART_2 = (
-    BINPUT, b'\x00',                            # store the string in memo 0
+    BINPUT, b'\x00',                        # store the string in memo 0
 
-    GLOBAL, b'operator\ngetitem\n',             # put operator.getitem on stack
-    BINPUT, b'\x01',                            # store it in memo 1
+    GLOBAL, b'builtins\nslice\n',           # put slice on stack
+    BINPUT, b'\x01',                        # store it in memo 1
 
-    GLOBAL, b'builtins\nslice\n',               # put slice on stack
-    BINPUT, b'\x02',                            # store it in memo 2
+    GLOBAL, b'operator\ngetitem\n',         # put operator.getitem on stack
+    BINPUT, b'\x02',                        # store it in memo 2
 
-    GLOBAL, b'operator\nadd\n',                 # put operator.add on stack
-    BINPUT, b'\x03',                            # store that in memo 3
+    GLOBAL, b'operator\nadd\n',             # put operator.add on stack
+    BINPUT, b'\x03',                        # store it in memo 3
 
-    BINGET, b'\x02',                            # load slice from memo 2
-    BININT1, b'\x00',                           # put 0 on stack
-    BININT1, b'\x04',                           # put 4 on stack
-    TUPLE2, REDUCE,                             # call --> slice(0, 4)
-    BINPUT, b'\x04',                            # store that in memo 4
+    BINGET, b'\x01',                        # load slice from memo 1
+    NONE,                                   # put None on stack
+    BININT1, b'\x04',                       # put 4 on stack
+    TUPLE2, REDUCE,                         # call --> slice(None, 4)
+    BINPUT, b'\x04',                        # store that in memo 4
 
-    BINGET, b'\x02',                            # load slice from memo 2
-    BININT1, b'\x04',                           # put 4 on stack
-    NONE,                                       # put None on stack
-    TUPLE2, REDUCE,                             # call --> slice(4, None)
-    BINPUT, b'\x05',                            # store that in memo 5
+    BINGET, b'\x01',                        # load slice from memo 1 again
+    BININT1, b'\x04',                       # put 4 on stack
+    NONE,                                   # put None on stack
+    TUPLE2, REDUCE,                         # call --> slice(4, None)
+    BINPUT, b'\x05',                        # store that in memo 5
 
-    BINGET, b'\x01',                            # load getitem from memo 1
-    BINGET, b'\x00',                            # load the string from memo 0
-    BINGET, b'\x04',                            # load slice(0, 4) from memo 4
-    TUPLE2, REDUCE,                             # tuple + call --> string[:4]
-    BINPUT, b'\x06',                            # store that in memo 6
+    BINGET, b'\x02',                        # load getitem from memo 2
+    BINGET, b'\x00',                        # load the string from memo 0
+    BINGET, b'\x04',                        # load slice(None, 4) from memo 4
+    TUPLE2, REDUCE,                         # tuple + call --> string[:4]
+    BINPUT, b'\x06',                        # store that in memo 6
 
-    BINGET, b'\x01',                            # load getitem from memo again
-    BINGET, b'\x00',                            # load the string from memo 0
-    BINGET, b'\x05',                            # slice(4, None) from memo 5
-    TUPLE2, REDUCE,                             # tuple + call --> string[4:]
-    BINPUT, b'\x07',                            # store that in memo 7
+    BINGET, b'\x02',                        # load getitem from memo 2 again
+    BINGET, b'\x00',                        # load the string from memo 0
+    BINGET, b'\x05',                        # slice(4, None) from memo 5
+    TUPLE2, REDUCE,                         # tuple + call --> string[4:]
+    BINPUT, b'\x07',                        # store that in memo 7
 
-    BINGET, b'\x03',                            # load operator.add from memo 3
-    BINGET, b'\x06',                            # load string[:4] from memo 6
-    BINGET, b'\x00',                            # load string from memo 0
-    TUPLE2, REDUCE,                             # build tuple, call +
-    BINPUT, b'\x08',                            # store that in memo 8
+    BINGET, b'\x03',                        # load operator.add from memo 3
+    BINGET, b'\x06',                        # load string[:4] from memo 6
+    BINGET, b'\x00',                        # load string from memo 0
+    TUPLE2, REDUCE,                         # build tuple, call +
+    BINPUT, b'\x08',                        # store that in memo 8
 
-    BINGET, b'\x03',                            # load operator.add from memo 3
-    BINGET, b'\x08',                            # string[:4] + string from memo
-    BINGET, b'\x07',                            # load string[4:] from memo 7
-    TUPLE2, REDUCE,                             # build tuple, call + again
+    BINGET, b'\x03',                        # load operator.add from memo 3
+    BINGET, b'\x08',                        # string[:4] + string from memo
+    BINGET, b'\x07',                        # load string[4:] from memo 7
+    TUPLE2, REDUCE,                         # build tuple, call + again
 
-    STOP,                                       # STOP
+    STOP,                                   # STOP
 )
 
 
@@ -93,46 +93,45 @@ def make_pickle(golfed):
 # can be harder to see what is the argument to what.  We also use MEMOIZE,
 # which is shorthand for BINPUT [next unused value].
 GOLFED_PART_2 = (
-    MEMOIZE,                                    # store the string in memo 0
+    MEMOIZE,                                # store the string in memo 0
 
     # Here we use an extra trick: since we load two globals from operator we
     # can save a few bytes by storing 'operator' only once, and calling
     # STACK_GLOBAL (which is otherwise marginally less efficient) twice.
     SHORT_BINSTRING, b'\x08', b'operator',
-    MEMOIZE,                                    # store it in memo 1
+    MEMOIZE,                                # store it in memo 1
 
     SHORT_BINSTRING, b'\x03', b'add',
-    STACK_GLOBAL,                               # operator.add
-    DUP,                                        # copy it
+    STACK_GLOBAL,                           # operator.add
+    DUP,                                    # copy it
 
-    BINGET, b'\x01',                            # operator
+    BINGET, b'\x01',                        # operator
     SHORT_BINSTRING, b'\x07', b'getitem',
-    STACK_GLOBAL,                               # operator.getitem
-    MEMOIZE,                                    # store it in memo 2
+    STACK_GLOBAL,                           # operator.getitem
+    MEMOIZE,                                # store it in memo 2
 
-    BINGET, b'\x00',                            # the string
-    GLOBAL, b'builtins\nslice\n',               # slice
-    MEMOIZE,                                    # store it in memo 3
-    # The canonical arg here would be 0, but None is valid and saves a byte.
-    NONE,                                       # None
-    BININT1, b'\x04',                           # 4
-    TUPLE2, REDUCE,                             # call slice --> slice(None, 4)
-    TUPLE2, REDUCE,                             # call getitem --> string[:4]
+    BINGET, b'\x00',                        # the string
+    GLOBAL, b'builtins\nslice\n',           # slice
+    MEMOIZE,                                # store it in memo 3
+    NONE,                                   # None
+    BININT1, b'\x04',                       # 4
+    TUPLE2, REDUCE,                         # call slice --> slice(None, 4)
+    TUPLE2, REDUCE,                         # call getitem --> string[:4]
 
-    BINGET, b'\x00',                            # the string
-    TUPLE2, REDUCE,                             # call add
+    BINGET, b'\x00',                        # the string
+    TUPLE2, REDUCE,                         # call add
 
-    BINGET, b'\x02',                            # getitem
-    BINGET, b'\x00',                            # the string
-    BINGET, b'\x03',                            # slice
-    BININT1, b'\x04',                           # 4
-    NONE,                                       # None
-    TUPLE2, REDUCE,                             # call slice --> slice(4, None)
-    TUPLE2, REDUCE,                             # call getitem --> string[4:]
+    BINGET, b'\x02',                        # getitem
+    BINGET, b'\x00',                        # the string
+    BINGET, b'\x03',                        # slice
+    BININT1, b'\x04',                       # 4
+    NONE,                                   # None
+    TUPLE2, REDUCE,                         # call slice --> slice(4, None)
+    TUPLE2, REDUCE,                         # call getitem --> string[4:]
 
-    TUPLE2, REDUCE,                             # call add
+    TUPLE2, REDUCE,                         # call add
 
-    STOP,                                       # STOP
+    STOP,                                   # STOP
 )
 
 
